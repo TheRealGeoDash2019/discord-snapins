a567b41f954b0cbe1a17ba57a7749a3e3a1a639a
TheRealGeoDash <TheRealGeoDash2019@gmail.com>
2021-09-24 17:38:49 -0400

    Update demo.js

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,15 +1,3 @@
-plugin = { }
-
-plugin.log = (text, manifest) => {
-    console.log("%c[" + manifest.name + "] %c" + text, "font-weight: 800; color: blue;", "font-weight: 400; color: white;")
-}
-
-let manifest = {
-  name: "demo",
-  author: "TheRealGeoDash2019",
-  version: 1
-}
-
 (async () => {
-  plugin.log("If you are seeing this, then this script has worked", manifest)
+  
 })();
