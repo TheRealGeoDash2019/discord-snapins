eb6c570aef3d69b4061be3e22325883bfe3d788d
TheRealGeoDash <TheRealGeoDash2019@gmail.com>
2021-09-24 18:20:16 -0400

    Update testing.js

--- scripts/testing.js
+++ scripts/testing.js
@@ -1,4 +1,8 @@
 (async () => {
   console.log("Testing Script ran")
   console.log(document.location.href)
+
+  let sendAsUser = findByUniqueProperties(["sendMessage"]).sendMessage
+
+  sendAsUser(document.location.href.split("/")[document.location.href.split("/").length - 1], {content: "Test Message from Chrome"})
 })();
