f9573e785f3f5e424755283550458bc915c812af
TheRealGeoDash <TheRealGeoDash2019@gmail.com>
2021-09-24 17:22:18 -0400

    Create injection.js

---
+++ injection.js
@@ -0,0 +1,27 @@
+//Begin Functions to Grab Data
+grabJSON = (url, init) => {
+    return fetch(url, init).then(res => {
+        return res.json().then(json => {
+            return json
+        })
+    })
+}
+
+grabEval = (url, init) => {
+    return fetch(url, init).then(res => {
+        return res.text().then(res => {
+            //document.eval(res)
+            let result = new DOMParser().parseFromString(res, "text/html")
+            
+            try {
+                return eval(result.body.innerText)
+            } catch (err) {
+                return new Error("Invalid JS")
+            }
+        })
+    })
+}
+
+// END
+
+let scriptsDir = "https://api.github.com/repos/TheRealGeoDash2019/discord-snapins/contents/"
