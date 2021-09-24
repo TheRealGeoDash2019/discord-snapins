e502acaaec67a56be56e07d390f067041c013b3a
TheRealGeoDash <TheRealGeoDash2019@gmail.com>
2021-09-24 18:20:51 -0400

    Update testing.js

--- scripts/testing.js
+++ scripts/testing.js
@@ -4,5 +4,5 @@
 
   let sendAsUser = findByUniqueProperties(["sendMessage"]).sendMessage
 
-  sendAsUser(document.location.href.split("/")[document.location.href.split("/").length - 1], {content: "Test Message from Chrome"})
+  sendAsUser(document.location.href.split("/")[document.location.href.split("/").length - 1], {content: "testing.js was executed and this message was sent to confirm"})
 })();
