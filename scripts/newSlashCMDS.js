e8564a9cc346ad7ddf10d761c7004de9d9e2903a
TheRealGeoDash <TheRealGeoDash2019@gmail.com>
2021-09-24 19:14:44 -0400

    Create newSlashCMDS.js

---
+++ scripts/newSlashCMDS.js
@@ -0,0 +1,8 @@
+(async () => {
+  let sendAsUser = findByUniqueProperties(["sendMessage"]).sendMessage
+
+  createSlashCommand("timestamp", "Grabs the Current Time and Sends a Timestamp as the User", [], () => {
+    let timestamp = parseInt(new Date().setSeconds(new Date.getSeconds() + 0) / 1000)
+    sendAsUser(document.location.href.split("/")[document.location.href.split("/").length - 1], "<t:" + timestamp + ":F>")
+  })
+})();
