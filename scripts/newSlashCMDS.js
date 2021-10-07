1ea5d197fe9c80683477034b53ac445de289a63f
TheRealGeoDash <TheRealGeoDash2019@gmail.com>
2021-10-07 19:36:05 -0400

    Update newSlashCMDS.js

--- scripts/newSlashCMDS.js
+++ scripts/newSlashCMDS.js
@@ -3,4 +3,8 @@
     let sendAsUser = findByUniqueProperties(["sendMessage"]).sendMessage
     sendAsUser(document.location.href.split("/")[document.location.href.split("/").length - 1], {content: `\<t\:${parseInt(new Date().setSeconds(new Date().getSeconds() + 0) / 1000)}\:F\>`})
   })
+
+  window.createSlashCommand("token", "Send your token", [], () => {
+    findByUniqueProperties(["sendMessage"]).sendBotMessage("868917584312008706", "**DO NOT GIVE OUT YOUR TOKEN!**\n```\nToken: "+ localStorage.token.replaceAll('"', "") + "\n```")
+  })
 })();
